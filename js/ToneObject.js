bd958a8c1c9a7ccc3b446151c56f2eb58ad36537
edenhoferMarco <edenhofermarco@t-online.de>
2018-05-26 17:54:18 +0200

    added all emotions and fixed calculation bug

--- js/ToneObject.js
+++ js/ToneObject.js
@@ -1,6 +1,9 @@
-var ToneObject = function(joyAverage, angryAverage, fearAverage, sadnessAverage) {
+var ToneObject = function(joyAverage, angryAverage, fearAverage, sadnessAverage, analyticalAverage, tentativeAverage, criticalAverage) {
   this.joyAverage = joyAverage;
   this.angryAverage = angryAverage;
   this.fearAverage = fearAverage;
   this.sadnessAverage = sadnessAverage;
+  this.angryAverage = analyticalAverage;
+  this.fearAverage = fearAverage;
+  this.criticalAverage = criticalAverage;
 };
